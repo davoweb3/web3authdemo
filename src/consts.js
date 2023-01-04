50f7351e3eaa3c5c7773212efafa4a8e7ae2872b
Davo Ramirez <105182325+davoweb3@users.noreply.github.com>
2023-01-04 16:57:35 -0500

    redes

--- src/consts.js
+++ src/consts.js
@@ -1,10 +1,9 @@
 export const WEB_3_AUTH_CLIENT_ID =
-    "BA3baHkRdoFyar-w41sHWktalThCcCAQb99Oeq0x_44eucU4d6g3XAyb27ZgoSV-BszsDXYns4bASuliGmngqxI";
-export const CHAIN_DISPLAY_NAME = "rinkeby";
+    "BKptcvMRPyShJO5lAa_eKDazfG3RSkQcRf-gLBEmMGhyDFEUf-rwE5vd534A8jz34jMB2HTnvr1Grm74t_BM3A0";
+export const CHAIN_DISPLAY_NAME = "MOONBEAM";
 export const CHAIN_BLOCK_EXPLORER = "https://rinkeby.etherscan.io/";
-export const CHAIN_ID = "0x4";
-export const CHAIN_RPC_TARGET =
-    "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
-export const CHAIN_TICKER = "ETH";
-export const CHAIN_TICKER_NAME = "Ethereum";
+export const CHAIN_ID = "0x507";
+export const CHAIN_RPC_TARGET = "https://moonbeam-alpha.api.onfinality.io/public";
+export const CHAIN_TICKER = "MOONBEAM";
+export const CHAIN_TICKER_NAME = "MOONBEAM";
 export const API_URL = "http://localhost:8080";
